642e8a5a44a43ad2a98a84d0388e2a545e87bb35
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@oslandia.com>
2018-03-12 12:37:56 +0100

    refactor(vector): add hole support
    This commits refactors Feature related code.
    
    It introduces a documented Feature object.
    GeoJSON2Feature returns either a Feature or an array of Feature (when parsing
    a FeatureCollection object).
    
    Geometry merges now only happens for multi-* geometries to simplify user
    handling of individual features.
    
    Geometry holes are now supported.
    
    And last but not least this commit fixes the geometry merge bug from issue 688.
    
    Fixes #688
    
    BREAKING CHANGE: feature collection are not merged anymore
    BREAKING CHANGE: GeoJSON2Feature now returns a Promise

--- test/featureUtils_unit_test.js
+++ test/featureUtils_unit_test.js
@@ -6,34 +6,49 @@ import Coordinates from '../src/Core/Geographic/Coordinates';
 const assert = require('assert');
 const geojson = require('../examples/geojson/simple.geojson.json');
 
-const feature = GeoJSON2Features.parse('EPSG:4326', geojson, undefined, { buildExtent: true });
+const promise = GeoJSON2Features.parse('EPSG:4326', geojson, undefined, { buildExtent: true });
 
 describe('FeaturesUtils', function () {
-    it('should correctly parse geojson', () => {
-        assert.equal(feature.geometries.length, 3);
+    it('should correctly parse geojson', (done) => {
+        promise.then((feature) => {
+            assert.equal(feature.length, 3);
+            done();
+        });
     });
-    it('should correctly compute extent geojson', () => {
-        assert.equal(feature.extent.west(), 0.30798339284956455);
-        assert.equal(feature.extent.east(), 2.4722900334745646);
-        assert.equal(feature.extent.south(), 42.91620643817353);
-        assert.equal(feature.extent.north(), 43.72744458647463);
+    it('should correctly compute extent geojson', (done) => {
+        promise.then((feature) => {
+            assert.equal(feature.extent.west(), 0.30798339284956455);
+            assert.equal(feature.extent.east(), 2.4722900334745646);
+            assert.equal(feature.extent.south(), 42.91620643817353);
+            assert.equal(feature.extent.north(), 43.72744458647463);
+            done();
+        });
     });
-    it('should correctly filter point', () => {
-        const coordinates = new Coordinates('EPSG:4326', 1.26, 42.9);
-        const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
-        assert.equal(filter.length, 1.0);
-        assert.equal(filter[0].type == 'point', 1.0);
+    it('should correctly filter point', (done) => {
+        promise.then((feature) => {
+            const coordinates = new Coordinates('EPSG:4326', 1.26, 42.9);
+            const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
+            assert.equal(filter.length, 1.0);
+            assert.equal(filter[0].feature.geometry.type == 'point', 1.0);
+            done();
+        });
     });
-    it('should correctly filter polygon', () => {
-        const coordinates = new Coordinates('EPSG:4326', 0.62, 43.52);
-        const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
-        assert.equal(filter.length, 1.0);
-        assert.equal(filter[0].type == 'polygon', 1.0);
+    it('should correctly filter polygon', (done) => {
+        promise.then((feature) => {
+            const coordinates = new Coordinates('EPSG:4326', 0.62, 43.52);
+            const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
+            assert.equal(filter.length, 1.0);
+            assert.equal(filter[0].feature.geometry.type == 'polygon', 1.0);
+            done();
+        });
     });
-    it('should correctly filter line', () => {
-        const coordinates = new Coordinates('EPSG:4326', 2.23, 43.39);
-        const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
-        assert.equal(filter.length, 1.0);
-        assert.equal(filter[0].type == 'linestring', 1.0);
+    it('should correctly filter line', (done) => {
+        promise.then((feature) => {
+            const coordinates = new Coordinates('EPSG:4326', 2.23, 43.39);
+            const filter = FeaturesUtils.filterFeaturesUnderCoordinate(coordinates, feature, 0.1);
+            assert.equal(filter.length, 1.0);
+            assert.equal(filter[0].feature.geometry.type == 'linestring', 1.0);
+            done();
+        });
     });
 });
